9848cecf78a48b28abc25811e0e0be37291a5825
thomashacker <EdwardSchmuhl@web.de>
2024-03-12 11:18:30 +0100

    Add gid

--- generator9000_app/app/layout.tsx
+++ generator9000_app/app/layout.tsx
@@ -1,7 +1,8 @@
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Suspense } from "react"
 import "./globals.css";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html data-theme="light" lang="en">
+      <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS_KEY ? process.env.GOOGLE_ANALYTICS_KEY : ""} />
       <body className={inter.className}>{children}</body>
     </html>
   );
