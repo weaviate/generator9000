ce4a2ab9bc853273a05b6da1091941d3bd0034da
thomashacker <EdwardSchmuhl@web.de>
2024-03-28 12:31:19 +0100

    Adjust templates

--- generator9000_app/app/components/types.ts
+++ generator9000_app/app/components/types.ts
@@ -134,6 +134,7 @@ export interface Template {
 
 export const cat_template: Template = {
     name: "Cat",
+    imageField: "img",
     prompt: "Craft a vivid and detailed description of a unique cat, incorporating elements such as its breed, distinctive physical characteristics, personality traits, and any remarkable story or background. Focus on painting a picture with words that brings the cat to life for the reader, highlighting its interactions with its environment, its preferences, and any quirks that make it stand out. Aim for a narrative that not only informs but also captivates, weaving in factual elements about the breed with creative storytelling to make the description as engaging and memorable as possible.",
     imagePrompt: "Create an image that portrays a cat in a way that transcends the ordinary, capturing its essence in a setting or with attributes that highlight its unique characteristics.This could involve illustrating the cat in an environment that reflects its natural habitat or personality, incorporating elements that showcase its breed's specific traits, or even imagining the cat in a scenario that tells a story. The goal is to generate an image that is not only realistic but also infused with intrigue and depth, making the viewer feel as if they are getting a glimpse into the cat's own world.The image should be detailed and expressive, suitable for educational or artistic purposes, with a focus on realism blended with creative flair.",
     datafields: [{
@@ -141,6 +142,12 @@ export const cat_template: Template = {
         name: "name",
         type: "text",
         values: [],
+    },
+    {
+        id: "img",
+        name: "img",
+        type: "text",
+        values: []
     },
     {
         id: "color",
@@ -181,46 +188,10 @@ export const cat_template: Template = {
     ]
 }
 
-export const dating_template: Template = {
-    name: "Dating App",
-    prompt: "Generate a persona for a dating site.",
-    imagePrompt: "Create a realistic portrait of an AI-generated person suitable for a dating platform mockup. The individual should appear approachable and attractive, with a warm, genuine smile that reflects a friendly and inviting personality. Ensure the lighting is natural and flattering, highlighting their best features. The background should be subtly blurred to keep the focus on the person, resembling a high-quality photograph one might use on a professional dating site. The person's attire should be casual yet put-together, conveying a sense of style and confidence. Aim for an image that embodies authenticity, with a person who looks like they could be someone's next great date. The overall impression should be positive, making the viewer feel drawn to the person's profile.",
-    datafields: [{
-        id: "name",
-        name: "name",
-        type: "text",
-        values: [],
-    },
-    {
-        id: "age",
-        name: "age",
-        type: "number",
-        values: [],
-    },
-    {
-        id: "country",
-        name: "country",
-        type: "text",
-        values: [],
-    },
-    {
-        id: "gender",
-        name: "gender",
-        type: "text",
-        values: ["male", "female", "other"],
-    },
-    {
-        id: "description",
-        name: "description",
-        type: "text",
-        values: [],
-    }
-    ]
-}
-
 export const electronics_template: Template = {
     name: "Old Electronics",
     prompt: "Generate a old electronic device for a retail shop. It can be old cameras, keyboards, typewriter, etc.",
+    imageField: "img",
     imagePrompt: "Generate an old electronic device image picture for a retail shop.",
     datafields: [{
         id: "name",
@@ -233,6 +204,12 @@ export const electronics_template: Template = {
         name: "age",
         type: "number",
         values: [],
+    },
+    {
+        id: "img",
+        name: "img",
+        type: "text",
+        values: []
     },
     {
         id: "electronic_type",
@@ -258,6 +235,7 @@ export const electronics_template: Template = {
 export const fastfood_template: Template = {
     name: "Fast Food",
     prompt: "Generate a fastfood item for a restaurant menu.",
+    imageField: "img",
     imagePrompt: "Create a highly detailed and realistic image of a single food item, presented as if for an menu. The item should be placed on a white plate against a pure white background, with no additional items, decorations, or visual elements present. The food item should be fully visible, capturing its texture and colors accurately, offering a clear and straightforward view.",
     datafields: [{
         id: "name",
@@ -270,6 +248,12 @@ export const fastfood_template: Template = {
         name: "price",
         type: "number",
         values: [],
+    },
+    {
+        id: "img",
+        name: "img",
+        type: "text",
+        values: []
     },
     {
         id: "ingredients",
@@ -294,6 +278,7 @@ export const fastfood_template: Template = {
 
 export const foodingredients_template: Template = {
     name: "Food Ingredients",
+    imageField: "img",
     prompt: "Generate a food ingredient item for an ecommerce site.",
     imagePrompt: "Create a highly detailed and realistic image of a single food item, presented as if for an e-commerce site. The item should be placed on a white plate against a pure white background, with no additional items, decorations, or visual elements present. The item should be fully visible, capturing its texture and colors accurately, offering a clear and straightforward view.",
     datafields: [{
@@ -307,6 +292,12 @@ export const foodingredients_template: Template = {
         name: "price",
         type: "number",
         values: [],
+    },
+    {
+        id: "img",
+        name: "img",
+        type: "text",
+        values: []
     },
     {
         id: "nutrients",
@@ -418,6 +409,7 @@ export const clothing_template: Template = {
 
 export const shoes_template: Template = {
     name: "Shoes",
+    imageField: "img",
     prompt: "Generate a fashion shoe item product in these categories:" + shoesSubCategories + " . Only use 1 or 2 colors, write short and precise descriptions. Be creative with the naming and the descriptions. Don't use words like Lunar, Midnight, Twilight, Ocean, Seaside or similar. ",
     imagePrompt: "Create a highly detailed and realistic image of a single shoe item, presented as if for an e-commerce website. The item should be either laid flat or hanging against a pure white background, with no additional items, decorations, or visual elements present. The shoe should be fully visible, capturing its texture and colors accurately, offering a clear and straightforward view suitable for online shopping.",
     datafields: [{
@@ -431,6 +423,12 @@ export const shoes_template: Template = {
         name: "brand",
         type: "text",
         values: brands
+    },
+    {
+        id: "img",
+        name: "img",
+        type: "text",
+        values: []
     },
     {
         id: "category",
@@ -479,6 +477,7 @@ export const shoes_template: Template = {
 
 export const accessoires_template: Template = {
     name: "Accessories",
+    imageField: "img",
     prompt: "Generate a Accessories item product in these categories:" + accessoriesSubCategories + " . Only use 1 or 2 colors, write short and precise descriptions. Be creative with the naming and the descriptions. Don't use words like Lunar, Midnight, Twilight, Ocean, Seaside or similar. ",
     imagePrompt: "Create a highly detailed and realistic image of a single accessories item with only one color combination, presented as if for an e-commerce website. The item should be either laid flat or hanging against a pure white background, with no additional items, decorations, or visual elements present. The item should be fully visible, capturing its texture and colors accurately, offering a clear and straightforward view suitable for online shopping.",
     datafields: [{
@@ -492,6 +491,12 @@ export const accessoires_template: Template = {
         name: "brand",
         type: "text",
         values: brands
+    },
+    {
+        id: "img",
+        name: "img",
+        type: "text",
+        values: []
     },
     {
         id: "category",
@@ -540,6 +545,7 @@ export const accessoires_template: Template = {
 
 export const outerwear_template: Template = {
     name: "Outerwear",
+    imageField: "img",
     prompt: "Generate a Outerwear item product in these categories:" + outerwearSubCategories + " . Only use 1 or 2 colors, write short and precise descriptions. Be creative with the naming and the descriptions. Don't use words like Lunar, Midnight, Twilight, Ocean, Seaside or similar. ",
     imagePrompt: "Create a highly detailed and realistic image of a single Outerwear item with only one color combination, presented as if for an e-commerce website. The item should be either laid flat or hanging against a pure white background, with no additional items, decorations, or visual elements present. The clothing should be fully visible, capturing its texture and colors accurately, offering a clear and straightforward view suitable for online shopping.",
     datafields: [{
@@ -553,6 +559,12 @@ export const outerwear_template: Template = {
         name: "brand",
         type: "text",
         values: brands
+    },
+    {
+        id: "img",
+        name: "img",
+        type: "text",
+        values: []
     },
     {
         id: "category",
@@ -602,6 +614,7 @@ export const outerwear_template: Template = {
 
 export const kitchenware_template: Template = {
     name: "Kitchenware",
+    imageField: "img",
     prompt: "Generate a kitchenware product likes plates and dishes. Be creative with the naming and the descriptions.",
     imagePrompt: "Create a highly detailed and realistic image of a single kitchenware item with one to two color combination, presented as if for an e-commerce website. The item should be either laid flat or hanging against a pure white background, with no additional items, decorations, or visual elements present. The clothing should be fully visible, capturing its texture and colors accurately, offering a clear and straightforward view suitable for online shopping.",
     datafields: [{
@@ -613,6 +626,12 @@ export const kitchenware_template: Template = {
     {
         id: "brand",
         name: "brand",
+        type: "text",
+        values: []
+    },
+    {
+        id: "img",
+        name: "img",
         type: "text",
         values: []
     },
@@ -658,6 +677,7 @@ export const kitchenware_template: Template = {
 export const book_template: Template = {
     name: "Books",
     prompt: "Generate a book",
+    imageField: "img",
     imagePrompt: "Create a highly detailed and realistic image of a book with a blurred background, candles and in a library.",
     datafields: [{
         id: "title",
@@ -668,6 +688,12 @@ export const book_template: Template = {
     {
         id: "author",
         name: "author",
+        type: "text",
+        values: []
+    },
+    {
+        id: "img",
+        name: "img",
         type: "text",
         values: []
     },
@@ -703,7 +729,7 @@ export interface GeneratedObject {
     [key: string]: string | null;
 }
 
-export const initial_templates: Template[] = [empty_template, foodingredients_template, book_template, kitchenware_template, outerwear_template, accessoires_template, fastfood_template, shoes_template, dating_template, cat_template, electronics_template, clothing_template]
+export const initial_templates: Template[] = [empty_template, foodingredients_template, book_template, kitchenware_template, outerwear_template, accessoires_template, fastfood_template, shoes_template, cat_template, electronics_template, clothing_template]
 
 export interface importAllPayload {
     prompt: string,
